f0550d3364f07f01c2f066c8d7130e8f45281e64
wintera1233 <yvonnez7@gapp.nthu.edu.tw>
2024-10-24 14:33:19 +0800

    usb settings

--- scripts/usb.sh
+++ scripts/usb.sh
@@ -7,3 +7,4 @@ cd $PACKAGES_PATH
 cd $PACKAGES_PATH/phidgets_api
 sudo cp debian/udev /etc/udev/rules.d/99-phidgets.rules
 sudo udevadm control --reload-rules && udevadm trigger
+sudo chmod a+rw /dev/bus/usb/001/
